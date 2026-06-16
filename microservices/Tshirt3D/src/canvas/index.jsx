511bfc04f5c61a4d20663c0a90f8218e66cb643d
Batman <spotifypremium29062005@gmail.com>
2026-06-16 14:40:13 -0500

    ajustando colores del modelo y agregando header

--- microservices/Tshirt3D/src/canvas/index.jsx
+++ microservices/Tshirt3D/src/canvas/index.jsx
@@ -1,5 +1,5 @@
 import { Canvas } from "@react-three/fiber";
-import { Environment, Center } from "@react-three/drei";
+import { Center } from "@react-three/drei";
 import { useSnapshot } from "valtio";
 
 import Shirt from "./Shirt";
@@ -18,11 +18,21 @@ const CanvasModel = () => {
       className="w-full max-w-full h-full transition-all ease-in"
     >
       <ambientLight intensity={0.5} />
-      <Environment preset="city" background={!snap.captureTransparent} />
+      {!snap.captureTransparent && <color attach="background" args={["#ffffff"]} />}
+
+      <directionalLight
+        castShadow
+        intensity={0.8}
+        position={[5, 10, 5]}
+        shadow-mapSize-width={1024}
+        shadow-mapSize-height={1024}
+        shadow-camera-near={0.5}
+        shadow-camera-far={50}
+      />
 
       <CameraRig>
         <Backdrop />
-        <Center>
+        <Center position={[0, 0.05, 0]}>
           <Shirt />
         </Center>
       </CameraRig>
